0831bad84f12afd5ff4e76554769f92a788fe123
dot-Justin <151479459+dot-Justin@users.noreply.github.com>
2026-06-07 17:36:04 -0500

    M1: C entry, AppMessage handshake, themed status window

--- src/c/wristcord.c
+++ src/c/wristcord.c
@@ -1,60 +1,57 @@
+// src/c/wristcord.c
 #include <pebble.h>
+#include "settings.h"
 
 static Window *s_window;
-static TextLayer *s_text_layer;
+static TextLayer *s_title, *s_state;
+static WristcordSettings s_settings;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+static void render_state(void) {
+  window_set_background_color(s_window, wc_theme_bg(&s_settings));
+  text_layer_set_text_color(s_title, s_settings.accent);
+  text_layer_set_text_color(s_state, wc_theme_fg(&s_settings));
+  text_layer_set_text(s_state, s_settings.has_token
+    ? "Connected.\nServers load in M3."
+    : "No token set.\nOpen Wristcord settings\nin the Pebble app.");
 }
 
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Up");
+static void inbox_received(DictionaryIterator *it, void *ctx) {
+  wc_settings_apply_from_msg(it, &s_settings);
+  wc_settings_save(&s_settings);
+  render_state();
 }
 
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Down");
+static void window_load(Window *w) {
+  Layer *root = window_get_root_layer(w);
+  GRect b = layer_get_bounds(root);
+  s_title = text_layer_create(GRect(0, 20, b.size.w, 30));
+  text_layer_set_text(s_title, "Wristcord");
+  text_layer_set_font(s_title, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text_alignment(s_title, GTextAlignmentCenter);
+  text_layer_set_background_color(s_title, GColorClear);
+  layer_add_child(root, text_layer_get_layer(s_title));
+
+  s_state = text_layer_create(GRect(8, 70, b.size.w - 16, b.size.h - 80));
+  text_layer_set_font(s_state, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_text_alignment(s_state, GTextAlignmentCenter);
+  text_layer_set_background_color(s_state, GColorClear);
+  layer_add_child(root, text_layer_get_layer(s_state));
+
+  render_state();
+}
+static void window_unload(Window *w) {
+  text_layer_destroy(s_title);
+  text_layer_destroy(s_state);
 }
 
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
-}
-
-static void prv_window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Press a button");
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
-}
-
-static void prv_window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
-}
-
-static void prv_init(void) {
+static void init(void) {
+  wc_settings_load(&s_settings);
+  app_message_register_inbox_received(inbox_received);
+  app_message_open(2048, 256);
   s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
-  window_set_window_handlers(s_window, (WindowHandlers) {
-    .load = prv_window_load,
-    .unload = prv_window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(s_window, animated);
+  window_set_window_handlers(s_window, (WindowHandlers){ .load = window_load, .unload = window_unload });
+  window_stack_push(s_window, true);
 }
+static void deinit(void) { window_destroy(s_window); }
 
-static void prv_deinit(void) {
-  window_destroy(s_window);
-}
-
-int main(void) {
-  prv_init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
-
-  app_event_loop();
-  prv_deinit();
-}
+int main(void) { init(); app_event_loop(); deinit(); }
